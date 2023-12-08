968abd234a21f2a31a80c6b9bf9ae9295419b009
thetai3092 <105540056+thetai3092@users.noreply.github.com>
2023-12-08 13:49:50 +0700

    Create Practice4.sql

---
+++ Practice4.sql
@@ -0,0 +1,58 @@
+--Ex1
+SELECT 
+COUNT(CASE
+WHEN device_type ='laptop' THEN 1
+END) AS laptop_views,
+COUNT(CASE
+WHEN device_type !='laptop' THEN 1
+END) AS mobile_views
+FROM viewership;
+--Ex2
+SELECT *,
+CASE 
+WHEN x+y>z and y+z>x and x+z>y THEN 'Yes' 
+ELSE 'No' 
+END AS triangle 
+FROM triangle; 
+--Ex3
+SELECT 
+CAST
+(
+COUNT(CASE
+WHEN call_category = ('n/a') OR call_category IS NULL THEN 1
+END)
+/COUNT(*)*100
+AS DECIMAL (12,1)
+)
+AS call_percentage
+FROM callers;
+--Ex4 
+--cách 1
+SELECT name FROM Customer         
+WHERE referee_id !=2
+OR referee_id IS null;
+-- cách 2
+SELECT name
+FROM Customer
+WHERE COALESCE(referee_id,'') != 2
+--Ex5
+SELECT 
+CASE
+WHEN survived = 0 THEN 0
+ELSE 1
+END AS survived,
+SUM(CASE
+WHEN pclass = 1 THEN 1 ELSE 0
+END) AS first_class,
+SUM(CASE
+WHEN pclass = 2 THEN 1 ELSE 0
+END) AS second_class,
+SUM(CASE
+WHEN pclass = 3 THEN 1 ELSE 0
+END) AS third_class
+FROM titanic
+GROUP BY 
+CASE
+WHEN survived = 0 THEN 0
+ELSE 1
+END
